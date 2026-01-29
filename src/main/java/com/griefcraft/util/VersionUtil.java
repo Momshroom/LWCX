2f49c59b482362630e632033ac7ef766c8b2ecc7
Christopher Bohn <pop4959@gmail.com>
2026-01-28 22:59:34 -0800

    Update VersionUtil to prepare for new Minecraft version scheme

--- src/main/java/com/griefcraft/util/VersionUtil.java
+++ src/main/java/com/griefcraft/util/VersionUtil.java
@@ -5,11 +5,30 @@ import org.bukkit.Bukkit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@SuppressWarnings("unused")
 public class VersionUtil {
+    private static final Pattern VERSION_PATTERN = Pattern.compile("(\\d+)\\.(\\d+)(\\.\\d+)?");
 
-    public static int getMinorVersion() {
-        Matcher versionCheck = Pattern.compile("\\d[.]\\d+").matcher(Bukkit.getVersion());
-        return versionCheck.find() ? Integer.parseInt(versionCheck.group().substring(2)) : -1;
+    public static int getMajorVersion() {
+        return Integer.parseInt(getVersionParts(Bukkit.getVersion())[0]);
     }
 
+    public static int getMinorVersion() {
+        return Integer.parseInt(getVersionParts(Bukkit.getVersion())[1]);
+    }
+
+    public static int getPatchVersion() {
+        return Integer.parseInt(getVersionParts(Bukkit.getVersion())[2]);
+    }
+
+    private static String[] getVersionParts(final String input) {
+        final Matcher versionMatcher = VERSION_PATTERN.matcher(input);
+        final String[] version = new String[]{"-1", "-1", "-1"};
+        if (!versionMatcher.find()) {
+            return version;
+        }
+        final String[] versionParts = versionMatcher.group().split("\\.");
+        System.arraycopy(versionParts, 0, version, 0, versionParts.length);
+        return version;
+    }
 }
